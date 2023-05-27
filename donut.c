07dea18a806bb994d03d093f4a08cf89c00ae94c
Glitchez <87347098+Glitchez-1984@users.noreply.github.com>
2023-05-26 20:16:33 -0400

    Create donut.c

---
+++ donut.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+    int k;
+    double sin(double), cos(double);
+
+    float A = 0, B = 0, i, j;
+    float z[1760];
+    char b[1760];
+    printf("\x1b[2J");
+    
+    for (;;) {
+        memset(b, 32, 1760);
+        memset(z, 0, 7040);
+        
+        for (j = 0; 6.28 > j; j += 0.07) {
+            for (i = 0; 6.28 > i; i += 0.02) {
+                float sini = sin(i);
+                float cosj = cos(j);
+                float sinA = sin(A);
+                float sinj = sin(j);
+                float cosA = cos(A);
+                float cosj2 = cosj + 2;
+                float mess = 1 / (sini * cosj2 * sinA + sinj * cosA + 5);
+                float cosi = cos(i);
+                float cosB = cos(B);
+                float sinB = sin(B);
+                float t = sini * cosj2 * cosA - sinj * sinA;
+                
+                int x = 40 + 30 * mess * (cosi * cosj2 * cosB - t * sinB);
+                int y = 12 + 15 * mess * (cosi * cosj2 * sinB + t * cosB);
+                int o = x + 80 * y;
+                int N = 8 * ((sinj * sinA - sini * cosj * cosA) * cosB - sini * cosj * sinA - sinj * cosA - cosi * cosj * sinB);
+                
+                if (22 > y && y > 0 && x > 0 && 80 > x && mess > z[o]) {
+                    z[o] = mess;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        }
+        
+        printf("\x1b[d");
+        
+        for (k = 0; 1761 > k; k++)
+            putchar(k % 80 ? b[k] : 10);
+        
+        A += 0.04;
+        B += 0.02;
+    }
+    return 0;
+}
